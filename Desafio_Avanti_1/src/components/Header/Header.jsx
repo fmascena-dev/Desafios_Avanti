634bffae0ffe87e69c6eab52c94d8a4dd76903a7
felipe mascena <felipe.mascena.dev@gmail.com>
2025-03-29 01:27:21 -0300

    feat: estruturando o Header e adicionando novas cores

--- Desafio_Avanti_1/src/components/Header/Header.jsx
+++ Desafio_Avanti_1/src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
-import { BsCart2 } from "react-icons/bs";
-import { FaRegUser } from "react-icons/fa";
-import { IoIosSearch } from "react-icons/io";
+import { BsCart2 } from 'react-icons/bs';
+import { FaRegUser } from 'react-icons/fa';
+import { IoIosSearch } from 'react-icons/io';
 
 export default function Header() {
   return (
@@ -14,26 +14,27 @@ export default function Header() {
       </section>
 
       <section>
-
-      <section>
-        <div>
-          <figure>
-            <img src="/logo_avanti.svg" alt="Logo da Avanti" />
-          </figure>
+        <section>
           <div>
-            <input type="search" placeholder="Digite o que você procura" />
-            <IoIosSearch />
+            <figure>
+              <img src="/logo_avanti.svg" alt="Logo da Avanti" />
+            </figure>
+            <div>
+              <input type="search" placeholder="Digite o que você procura" />
+              <IoIosSearch />
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
 
-      <section>
-        <FaRegUser />
-        <p>Olá, Nome cliente!</p>
-        <BsCart2 />
+        <section className="section-user">
+          <FaRegUser />
+          <p className="user">Olá, Nome cliente!</p>
+          <div className="car">
+            <BsCart2 />
+            <span className="car-itens">2</span>
+          </div>
+        </section>
       </section>
-      </section>
-
     </header>
   );
 }
